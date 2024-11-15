bc3c39d303f4a277a49ab5b27224c58b1129fd4c
Lalan Kumar <21e51a6932@hitam.org>
2024-11-15 18:30:25 +0530

    model trainer completed

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.components.data_validation import DataValidation
 from networksecurity.components.data_transformation import DataTransformation
+from networksecurity.components.model_trainer import ModelTrainer
 from networksecurity.exception.exception import NetworkSecurityException
-from networksecurity.entity.config_entity import DataIngestionConfig, DataValidationConfig, DataTransformationConfig
+from networksecurity.entity.config_entity import DataIngestionConfig, DataValidationConfig, DataTransformationConfig, ModelTrainerConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
 from networksecurity.logging.logger import logging
 import sys
@@ -31,6 +32,12 @@ if __name__=="__main__":
         data_transformation_artifact=data_transformation.initiate_data_transformation()
         logging.info("Data Transformation Complete")
         print(data_transformation_artifact)
+        
+        logging.info("Model Training Started")
+        model_trainer_config=ModelTrainerConfig(trainingpipelinelineconfig)
+        model_trainer=ModelTrainer(model_trainer_config=model_trainer_config,data_transformation_artifact=data_transformation_artifact)
+        model_trainer_artifact=model_trainer.initiate_model_trainer()
+        logging.info("Model Training Artifact created")
          
     except Exception as e:
         raise NetworkSecurityException(e,sys)
