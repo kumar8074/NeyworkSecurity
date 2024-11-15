742fd71f059313c45656f7b7cd1ea9cfb95d6b95
Lalan Kumar <21e51a6932@hitam.org>
2024-11-15 19:50:31 +0530

    model pusher completed

--- networksecurity/components/data_transformation.py
+++ networksecurity/components/data_transformation.py
@@ -81,6 +81,9 @@ class DataTransformation:
             save_numpy_array_data(self.data_transformation_config.transformed_test_file_path, array=test_arr,)
             save_object(self.data_transformation_config.transformed_object_file_path, preprocessor_object,)
             
+            # For saving to final_models
+            save_object("final_models/preprocessor.pkl",preprocessor_object,)
+            
             # Preparing Artifacts
             data_transformation_artifact=DataTransformationArtifact(
                 transformed_object_file_path=self.data_transformation_config.transformed_object_file_path,
--- networksecurity/components/model_trainer.py
+++ networksecurity/components/model_trainer.py
@@ -110,6 +110,9 @@ class ModelTrainer:
         Network_Model=NetworkModel(preprocessor=preprocessor, model=best_model)
         save_object(self.model_trainer_config.trained_model_file_path, obj=NetworkModel)
         
+        # To save to final_models
+        save_object("final_models/model.pkl",best_model)
+        
         # Model Trainer Artifact
         model_trainer_artifact=ModelTrainerArtifact(trained_model_file_path=self.model_trainer_config.trained_model_file_path,
                              train_metric_artifact=classification_train_metric,
